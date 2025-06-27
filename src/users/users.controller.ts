37e3185b520a1b4f49ff6824d2f3ef32f36ab38f
Amber Chiu <suzumiya1235@gmail.com>
2025-06-27 20:34:23 +0800

    feat: Implement reports and users modules with controllers, services, and entities

---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log('body', body);
+
+    return { message: 'User created successfully' };
+  }
+}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: true })
+  password?: string;
+
+  @Column({ default: false })
+  isVerified: boolean;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+}
