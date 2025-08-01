f4c8e37be0474f3f367a50f83956c82245f6115b
Amber Chiu <suzumiya1235@gmail.com>
2025-08-01 09:50:49 +0800

    feat: Integrate ConfigModule for environment configuration and update database connection to PostgreSQL

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { AppConfig } from './configs/app.config';
+import { configType } from './configs/config.type';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService<configType>,
+  ) {}
 
   @Get()
   getHello(): string {
+    const prefix = this.configService.get<AppConfig>('app').messagePrefix;
+    console.log(`Using message prefix: ${prefix}`);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,71 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/users.entity';
-import { Reports } from './reports/reports.entity';
+import { appConfig } from './configs/app.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { ReportsModule } from './modules/reports/reports.module';
+import { UsersModule } from './modules/users/users.module';
+
+// const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Reports],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      load: [appConfig],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().port().default(3000),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USER'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [__dirname + '/../**/*.entity.js'],
+        };
+      },
     }),
     UsersModule,
+    AuthModule,
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(
+  //       cookieSession({
+  //         keys: this.configService.get<string>('COOKIE_KEYS')?.split(',') || [],
+  //       }),
+  //     )
+  //     .forRoutes('*');
+  // }
+}
