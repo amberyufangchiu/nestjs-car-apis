ae663dbb5d57032af060d0bb9d20c12cfad449c4
Amber Chiu <suzumiya1235@gmail.com>
2025-08-14 08:14:10 +0800

    feat: Add PaginationDto and PaginationResponse interface for handling paginated data

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,26 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+
+export class PaginationDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  limit: number = 10;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  offset: number = 0;
+}
+
+export interface PaginationResponse<T> {
+  data: T[];
+  meta: {
+    total: number;
+    limit: number;
+    offset: number;
+  };
+}
